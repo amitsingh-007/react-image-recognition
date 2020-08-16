8ff39a622b4241ab05e9a9f70dc7dc1bba3233a2
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-08-16 16:37:14 +0530

    WIP

--- src/containers/ImageRecognition.js
+++ src/containers/ImageRecognition.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Input } from "../components/Input";
 import { Box } from "@material-ui/core";
 import { UserImage } from "../components/UserImage";
+import { Results } from "../components/Results";
 
 export const ImageRecognition = () => {
   const [imageUrl, setImageUrl] = useState("");
@@ -12,6 +13,7 @@ export const ImageRecognition = () => {
       <Heading />
       <Input setImageUrl={setImageUrl} imageUrl={imageUrl} />
       <UserImage imageUrl={imageUrl} />
+      <Results />
     </Box>
   );
 };
